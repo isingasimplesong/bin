cce899ddd52b814aaf4cbdc541b87faa13381149
mathieu <mathieu@2027a.net>
2024-10-11 08:52:38 -0400

    github init repos scripts

--- github-init-repo-priv.sh
+++ github-init-repo-priv.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 project_name=$(basename "$(pwd)")
-gh repo create "$project_name" --public --source=. --remote=origin &&
+git init &&
+git add .
+git commit -m "initial commit" &&
+gh repo create "$project_name" --private --source=. --remote=origin &&
 git push --set-upstream origin main &&
 echo "Nouveau dépot github crée."
