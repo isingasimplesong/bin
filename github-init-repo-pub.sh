8eccc6dfe0055377184ca694d76941a1fa3b2d9e
mathieu <mathieu@2027a.net>
2024-10-11 22:01:49 -0400

    &&

--- github-init-repo-pub.sh
+++ github-init-repo-pub.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 project_name=$(basename "$(pwd)")
 git init &&
-git add .
+git add . &&
 git commit -m "initial commit" &&
 gh repo create "$project_name" --public --source=. --remote=origin &&
 git push --set-upstream origin main &&
